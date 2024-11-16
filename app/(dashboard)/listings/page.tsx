303565d61adf8fff648d4a2e60d97d5bfab6ea7e
nerwinalamas <nerwinalamas@gmail.com>
2024-11-16 19:26:53 +0800

    feat: listings page

---
+++ app/(dashboard)/listings/page.tsx
@@ -0,0 +1,11 @@
+import ListingsProperties from "./_components/listings-properties";
+
+const Listings = () => {
+    return (
+        <div className="min-h-screen rounded-md md:p-4">
+            <ListingsProperties />
+        </div>
+    );
+};
+
+export default Listings;
